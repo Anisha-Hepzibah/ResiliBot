0e2ea3a87eb66b06b96fdcb14efe8d4071813cdf
Anisha Priyadarshini R <133488209+Anisha-Hepzibah@users.noreply.github.com>
2025-07-17 13:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ if submitted and user_input:
         else:
             tag = sorted_conf[0][0]
             top3_conf = [
-                [intent, round(float(score*100, 2))] for intent, score in sorted_conf[:3]
+                [intent, round(float(round(score*100, 2)))] for intent, score in sorted_conf[:3]
             ]
 
         # Find impactful negative words
